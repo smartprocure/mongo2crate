ea33e0b0bb2aa32da7a82d3158f214652b2d61b0
David Sargeant <david@dsargeant.com>
2022-08-08 13:31:42 -0400

    MongoDB to Crate.

---
+++ src/convertSchema.test.ts
@@ -0,0 +1,102 @@
+import { convertSchema } from './convertSchema'
+
+const obj = {
+  bsonType: 'object',
+  additionalProperties: false,
+  required: ['name', 'type'],
+  properties: {
+    _id: {
+      bsonType: 'objectId',
+    },
+    name: { bsonType: 'string' },
+    numberOfEmployees: {
+      bsonType: 'string',
+      enum: ['1 - 5', '6 - 20', '21 - 50', '51 - 200', '201 - 500', '500+'],
+    },
+    notificationPreferences: {
+      bsonType: 'array',
+      items: {
+        bsonType: 'string',
+        enum: [
+          'newMatchingRFQ',
+          'activityOnRFQWhereParticipant',
+          'activityOnRFQBySameOrgUsers',
+        ],
+      },
+    },
+    addresses: {
+      bsonType: 'array',
+      items: {
+        bsonType: 'object',
+        additionalProperties: false,
+        properties: {
+          address: {
+            bsonType: 'object',
+            additionalProperties: false,
+            properties: {
+              street: { bsonType: 'string' },
+              city: { bsonType: 'string' },
+              county: { bsonType: 'string' },
+              state: { bsonType: 'string' },
+              zip: { bsonType: 'string' },
+              country: { bsonType: 'string' },
+            },
+          },
+          name: { bsonType: 'string' },
+          isPrimary: { bsonType: 'bool' },
+        },
+      },
+    },
+    integrations: {
+      bsonType: 'object',
+      additionalProperties: true,
+      properties: {
+        stripe: {
+          bsonType: 'object',
+          additionalProperties: true,
+          properties: {
+            priceId: {
+              bsonType: 'number',
+            },
+            subscriptionStatus: {
+              bsonType: 'string',
+            },
+          },
+        },
+      },
+    },
+  },
+}
+
+describe('convertSchema', () => {
+  it('should convert the schema', () => {
+    expect(convertSchema(obj, 'foo')).toEqual(`CREATE TABLE doc.foo (
+  "id" TEXT PRIMARY KEY,
+  "name" TEXT,
+  "numberOfEmployees" TEXT,
+  "notificationPreferences" ARRAY (
+    TEXT
+  ),
+  "addresses" ARRAY (
+    OBJECT(STRICT) AS (
+      "address" OBJECT(STRICT) AS (
+        "street" TEXT,
+        "city" TEXT,
+        "county" TEXT,
+        "state" TEXT,
+        "zip" TEXT,
+        "country" TEXT
+      ),
+      "name" TEXT,
+      "isPrimary" BOOLEAN
+    )
+  ),
+  "integrations" OBJECT(STRICT) AS (
+    "stripe" OBJECT(STRICT) AS (
+      "priceId" INTEGER,
+      "subscriptionStatus" TEXT
+    )
+  )
+)`)
+  })
+})
---
+++ src/convertSchema.ts
@@ -0,0 +1,84 @@
+import _ from 'lodash/fp.js'
+import { Node, walk } from 'obj-walker'
+import util from 'node:util'
+
+const arrayStartsWith = (arr: any[], startsWith: any[]) => {
+  for (let i = 0; i < startsWith.length; i++) {
+    if (arr[i] !== startsWith[i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+const bsonTypeToSQL: Record<string, string> = {
+  objectId: 'TEXT',
+  string: 'TEXT',
+  date: 'TIMESTAMP WITH TIME ZONE',
+  number: 'INTEGER',
+  bool: 'BOOLEAN',
+}
+
+const convertType = (bsonType: string | string[]) => {
+  if (Array.isArray(bsonType)) {
+    bsonType = bsonType[0]
+  }
+  return bsonTypeToSQL[bsonType]
+}
+
+const showCommaIf = (cond: boolean) => (cond ? ',' : '')
+
+const _convertSchema = (nodes: Node[], spacing = ''): string => {
+  let returnVal = ''
+  while (true) {
+    const node = nodes[0]
+    if (!node) {
+      return returnVal
+    }
+    const isPrimaryKey = _.equals(node.path, ['_id'])
+    const field = isPrimaryKey
+      ? '"id" '
+      : node.key === '_items'
+      ? ''
+      : `"${node.key}" `
+    // Create table
+    if (node.isRoot) {
+      return (
+        'CREATE TABLE doc.%s (\n' + _convertSchema(nodes.slice(1), '  ') + ')'
+      )
+    }
+    // Scalar fields
+    if (node.isLeaf) {
+      const comma = showCommaIf(nodes.length > 1)
+      const sqlType = convertType(node.val.bsonType)
+      const primary = isPrimaryKey ? ' PRIMARY KEY' : ''
+      returnVal += `${spacing}${field}${sqlType}${primary}${comma}\n`
+      nodes = nodes.slice(1)
+    }
+    // Arrays and objects
+    else {
+      const index = _.findLastIndex(
+        (n) => arrayStartsWith(n.path, node.path),
+        nodes
+      )
+      const childNodes = nodes.slice(1, index + 1)
+      const newSpacing = spacing + '  '
+      const comma = showCommaIf(nodes.length - childNodes.length > 1)
+      const sqlType =
+        node.val.bsonType === 'array' ? 'ARRAY' : 'OBJECT(STRICT) AS'
+      returnVal +=
+        `${spacing}${field}${sqlType} (\n` +
+        _convertSchema(childNodes, newSpacing) +
+        `${spacing})${comma}\n`
+      nodes = nodes.slice(index + 1)
+    }
+  }
+}
+
+export const convertSchema = (jsonSchema: object, tableName: string) => {
+  const traverse = (x: any) => x.properties || (x.items && { _items: x.items })
+  const nodes = walk(jsonSchema, { traverse })
+  const sqlSchema = _convertSchema(nodes)
+  return util.format(sqlSchema, tableName)
+}
