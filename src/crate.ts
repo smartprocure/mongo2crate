023a7afef427fbefa4d79062703f31115dcd3a9e
David Sargeant <david@dsargeant.com>
2022-08-09 11:54:44 -0400

    Upsert.

--- src/crate.ts
+++ src/crate.ts
@@ -25,6 +25,19 @@ interface Options {
 const maybeShowColTypes = (endpoint: string, coltypes: boolean) =>
   coltypes ? `${endpoint}?types` : endpoint
 
+export const getInsertColsAndPlaceholders = (obj: object) => {
+  const keys = Object.keys(obj)
+  const columns = keys.map((col) => `"${col}"`).join(',')
+  const placeholders = _.repeat(keys.length - 1, '?,') + '?'
+  return { columns, placeholders }
+}
+
+export const getUpdateColsAndPlaceholders = (obj: object) => {
+  const keys = Object.keys(obj)
+  const assignments = keys.map((col) => `"${col}" = ?`).join(',')
+  return { assignments }
+}
+
 export const crate = (sqlEndpoint = 'http://localhost:4200/_sql') => {
   const query = (sql: string, { args, coltypes = false }: Options) =>
     fetch(maybeShowColTypes(sqlEndpoint, coltypes), {
@@ -33,11 +46,37 @@ export const crate = (sqlEndpoint = 'http://localhost:4200/_sql') => {
       headers: { 'Content-Type': 'application/json' },
     }).then((res) => res.json() as Promise<QueryResult>)
 
+  const insert = (tableName: string, record: object) => {
+    const { columns, placeholders } = getInsertColsAndPlaceholders(record)
+    const sql = `INSERT INTO doc.${tableName} (${columns}) VALUES (${placeholders})`
+    return fetch(sqlEndpoint, {
+      method: 'post',
+      body: JSON.stringify({
+        stmt: sql,
+        args: Object.values(record),
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => res.json() as Promise<QueryResult>)
+  }
+
+  const upsert = (tableName: string, record: object, update: object) => {
+    const { columns, placeholders } = getInsertColsAndPlaceholders(record)
+    const {assignments} = getUpdateColsAndPlaceholders(update)
+    const sql = `INSERT INTO doc.${tableName} (${columns}) VALUES (${placeholders})
+    ON CONFLICT (id) DO UPDATE SET ${assignments}`
+    return fetch(sqlEndpoint, {
+      method: 'post',
+      body: JSON.stringify({
+        stmt: sql,
+        args: {...Object.values(record), ...Object.values(update)},
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => res.json() as Promise<QueryResult>)
+  }
+
   const bulkInsert = (tableName: string, records: object[]) => {
-    const keys = Object.keys(records[0])
-    const columns = keys.map((col) => `"${col}"`).join(',')
-    const placeholders = _.repeat(keys.length - 1, '?,') + '?'
-    const sql = `INSERT INTO doc.${tableName} (${columns}) VALUES(${placeholders})`
+    const { columns, placeholders } = getInsertColsAndPlaceholders(records[0])
+    const sql = `INSERT INTO doc.${tableName} (${columns}) VALUES (${placeholders})`
     const bulkArgs = records.map(Object.values)
     return fetch(sqlEndpoint, {
       method: 'post',
@@ -49,5 +88,7 @@ export const crate = (sqlEndpoint = 'http://localhost:4200/_sql') => {
     }).then((res) => res.json() as Promise<BulkQueryResult>)
   }
 
-  return { query, bulkInsert }
+  return { query, insert, upsert, bulkInsert }
 }
+
+export type Crate = ReturnType<typeof crate>
